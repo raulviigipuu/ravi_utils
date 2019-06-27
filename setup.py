7f4cddc7b6eaba463e0057f3c2f43727c2095262
Raul Viigipuu <raul@viigipuu.ee>
2019-06-27 09:11:02 +0300

    0.1.6 changes

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
 """Setup for package """
 
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='ravi_utils',
-    version='0.1.4',
-    description='Common functions package for simple scripts',
+    version='0.1.6',
     author='Raul Viigipuu',
     author_email='raul@viigipuu.ee',
+    description='Common functions package for simple scripts',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/raulviigipuu/ravi_utils',
-    py_modules=['ravi_utils'],
-    download_url='https://github.com/raulviigipuu/ravi_utils/archive/0.1.4.tar.gz',
-    keywords=['utils'],
-    classifiers=[]
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
